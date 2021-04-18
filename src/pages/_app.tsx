b14f4a413123714b152e77702fc3dcc986672258
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-04-18 16:49:51 -0300

    Update project info

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>NextJS Boilerplate With TailwindCSS</title>
+        <title>Workout Log</title>
         <link rel="shortcut icon" href="/tailwindcss.png" />
         <link rel="apple-touch-icon" href="/tailwindcss.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="NextJS Boilerplate with TailwindCSS"
+          content="A place for you to store your workouts"
         />
       </Head>
 
