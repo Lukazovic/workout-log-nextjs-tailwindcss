3990880e805ca6ae5529ce81efc320dd57c67f49
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-04-21 09:52:19 -0300

    Refactor Heading tests

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -3,72 +3,59 @@ import { render, screen } from '@testing-library/react'
 import Heading from '.'
 
 describe('<Heading />', () => {
-  it('should render only a h1 heading by default', () => {
-    const { container } = render(<Heading>Heading text</Heading>)
+  it('should render a h1 heading by default', () => {
+    render(<Heading>Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 1, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h1')).toHaveLength(1)
-    expect(container.querySelectorAll('h2')).toHaveLength(0)
-    expect(container.querySelectorAll('h3')).toHaveLength(0)
-    expect(container.querySelectorAll('h4')).toHaveLength(0)
-    expect(container.querySelectorAll('h5')).toHaveLength(0)
-    expect(container.querySelectorAll('h6')).toHaveLength(0)
-
-    expect(container.parentElement).toMatchSnapshot()
   })
 
-  it('should render only a h2 heading', () => {
-    const { container } = render(<Heading as="h2">Heading text</Heading>)
+  it('should render a h2 heading', () => {
+    render(<Heading as="h2">Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 2, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h2')).toHaveLength(1)
   })
 
-  it('should render only a h3 heading', () => {
-    const { container } = render(<Heading as="h3">Heading text</Heading>)
+  it('should render a h3 heading', () => {
+    render(<Heading as="h3">Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 3, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h3')).toHaveLength(1)
   })
 
-  it('should render only a h4 heading', () => {
-    const { container } = render(<Heading as="h4">Heading text</Heading>)
+  it('should render a h4 heading', () => {
+    render(<Heading as="h4">Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 4, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h4')).toHaveLength(1)
   })
 
-  it('should render only a h5 heading', () => {
-    const { container } = render(<Heading as="h5">Heading text</Heading>)
+  it('should render a h5 heading', () => {
+    render(<Heading as="h5">Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 5, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h5')).toHaveLength(1)
   })
 
-  it('should render only a h6 heading', () => {
-    const { container } = render(<Heading as="h6">Heading text</Heading>)
+  it('should render a h6 heading', () => {
+    render(<Heading as="h6">Heading text</Heading>)
 
     expect(
-      screen.getByRole('heading', { name: /heading text/i })
+      screen.getByRole('heading', { level: 6, name: /heading text/i })
     ).toBeInTheDocument()
-    expect(container.querySelectorAll('h6')).toHaveLength(1)
   })
 
   it('should render with dark color by default', () => {
     render(<Heading>Heading text</Heading>)
 
     expect(
-      screen.getByRole(Heading, { name: /heading text/i })
+      screen.getByRole('heading', { level: 1, name: /heading text/i })
     ).toHaveAttribute('class', 'font-bold text-4xl text-gray-900')
   })
 
@@ -76,7 +63,7 @@ describe('<Heading />', () => {
     render(<Heading color="light">Heading text</Heading>)
 
     expect(
-      screen.getByRole(Heading, { name: /heading text/i })
+      screen.getByRole('heading', { level: 1, name: /heading text/i })
     ).toHaveAttribute('class', 'font-bold text-4xl text-gray-100')
   })
 })
