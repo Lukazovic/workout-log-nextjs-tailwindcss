50a52a965a646878c1d1e38e2b53308dca296b3c
Lukazovic <lucas10.vieira.lv@gmail.com>
2021-04-21 21:14:34 -0300

    Add totalDurantion and dateToUtcTimestamp func calc

--- src/services/tools/general/index.ts
+++ src/services/tools/general/index.ts
@@ -1,5 +1,28 @@
+import { ExerciseProps } from 'components/WorkoutTable/TableData'
+
+const ONE_MINUTE_IN_SECONDS = 60
+const ONE_SECOND_IN_MILLISECOND = 1000
+
+const minutesToMilliseconds = (minutes: number) =>
+  minutes * ONE_MINUTE_IN_SECONDS * ONE_SECOND_IN_MILLISECOND
+
+const dateToUtcTimestamp = (timestamp: number) => {
+  const currentTimezoneInMinutes = new Date().getTimezoneOffset()
+
+  return timestamp + minutesToMilliseconds(currentTimezoneInMinutes)
+}
+
 const isOdd = (number: number) => number % 2
 
+const totalDuration = (exercises: ExerciseProps[]) =>
+  exercises.reduce(
+    (accumulator, exercise) => accumulator + exercise.duration,
+    0
+  )
+
 export default {
-  isOdd
+  minutesToMilliseconds,
+  dateToUtcTimestamp,
+  isOdd,
+  totalDuration
 }
